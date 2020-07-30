0cda46014d2985fd6a3c87040db38a182e15fe9f
xzj <123456@qq.com>
2020-07-30 16:55:58 +0800

    完成登录注册功能

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,5 @@
+$.ajaxPrefilter(function (option) {
+
+    option.url = 'http://ajax.frontend.itheima.net' + option.url
+    console.log(option.url);
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -2,9 +2,71 @@ $(function () {
     $('#link_reg').on('click', function () {
         $('.login-box').hide()
         $('.reg-box').show()
+
     })
     $('#link_login').on('click', function () {
         $('.login-box').show()
         $('.reg-box').hide()
+
     })
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6-12位,且不能出现空格']
+        ,
+        //校验两次密码是否一致的规则
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码输入不一致!'
+            }
+
+        }
+
+    })
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.ajax({
+            method: 'post',
+            url: '/api/reguser',
+            data: data,
+
+            success: function (res) {
+                if (res.status != 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功,请登录!')
+                //触动a链接的点击行为
+                $('#link_login').click()
+                $('#form_reg')[0].reset()
+
+            }
+        })
+    })
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'post',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status != 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+
+            }
+        })
+
+    })
+
+
+
+
 })
